cb66d994a143a3c460173807294ad53f1d46611d
IsanorLopez <isanoralejandro@gmail.com>
2025-04-05 12:23:03 -0600

    Feat: Rendering dynamic values from controller to view using thymeleaf

--- web-api/src/main/java/com/isanor/webapi/controller/UserController.java
+++ web-api/src/main/java/com/isanor/webapi/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.isanor.webapi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+
+        model.addAttribute("Title", "Spring Boot Application");
+        model.addAttribute("Name", "Isanor Lopez");
+
         return "details";
     }
 
