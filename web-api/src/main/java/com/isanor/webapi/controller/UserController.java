225d8db4cded7ed16ecec5f60bde45fbb18fb31b
IsanorLopez <isanoralejandro@gmail.com>
2025-04-05 11:43:30 -0600

    Feat: Details endpoint showing a HTML view

---
+++ web-api/src/main/java/com/isanor/webapi/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.isanor.webapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details() {
+        return "details";
+    }
+
+}
