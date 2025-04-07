2647d0554c995806465af4936b0b08eeeed889d6
Isanor Lopez <isanorlopez@MacBook-Pro-de-Isanor-3.local>
2025-04-07 10:07:45 -0600

    Feat: User model implemented for mapping structure

--- web-api/src/main/java/com/isanor/webapi/controller/UserRestController.java
+++ web-api/src/main/java/com/isanor/webapi/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package com.isanor.webapi.controller;
 
+import com.isanor.webapi.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,12 @@ public class UserRestController {
     @GetMapping("/details")
     public Map<String, Object> details() {
 
+        User user = new User("Isanor", "Lopez", "isanor@isanor.com");
+
         Map<String, Object> response = new HashMap<>();
 
         response.put("Title", "Spring Boot Application");
-        response.put("Name", "Isanor Lopez");
+        response.put("User", user);
 
         return response;
     }
