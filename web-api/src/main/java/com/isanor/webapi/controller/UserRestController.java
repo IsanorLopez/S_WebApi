052b787ce50750ec71c154ca73b693e91679cc3f
Isanor Lopez <isanorlopez@MacBook-Pro-de-Isanor-3.local>
2025-04-07 10:56:01 -0600

    Feat: Returning a user list endpoint

--- web-api/src/main/java/com/isanor/webapi/controller/UserRestController.java
+++ web-api/src/main/java/com/isanor/webapi/controller/UserRestController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +24,22 @@ public class UserRestController {
         userDto.setUser(user);
 
         return userDto;
+    }
+
+    @GetMapping("/details/list")
+    public List<User> listDetails() {
+
+        User user1 = new User("IsanorX", "Lopez", "isanor@isanor.com");
+        User user2 = new User("Alejandro", "Molina", "alejandro@isanor.com");
+        User user3 = new User("Carlos", "Hernandez", "carlos@isanor.com");
+
+        //List<User> users = new ArrayList<>();
+
+        //users.add(user1);
+        //users.add(user2);
+        //users.add(user3);
+
+        return Arrays.asList(user1, user2, user3);
     }
 
 }
